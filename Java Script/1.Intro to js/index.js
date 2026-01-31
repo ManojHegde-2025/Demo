a6de0be0ca9579e015b343eaea115e3967099b5e
nu25c32 <nu25c32@nmait.in>
2026-01-31 19:33:18 +0530

    javascript

---
+++ Java Script/1.Intro to js/index.js
@@ -0,0 +1,12 @@
+let name="manoj";
+        let age = "19";
+        console.log("my name is",name);
+        console.log("and my age is ",age);
+        
+        let a = 5;
+        let b = 5;
+        let c = a+b;
+        console.log("sum of a",a,"and b",b, "is = ",c);
+        console.log(c)
+        console.log(c-5);
+        
---
+++ Java Script/4.If-else-conditions/script.js
@@ -0,0 +1,35 @@
+const a = 170;
+// if (a >=18)
+// console.log("you are eligible to vote");
+// else
+//     console.log("you are not eligible to vote");
+
+//nested if
+//if enclosed within another if
+
+// if(a>=18 && a<60){
+//     console.log("you are eligible to vote");
+// }
+
+// else if (a>=60){
+//     console.log("you are a senior citizen -- online voting is available for you");
+    
+// }
+
+    
+
+//else if ladder
+//chain of if else if else statement
+
+// if(a>=18){
+//      if (a>=60){
+//     console.log("you are a senior citizen -- online voting is available for you");   
+// }
+//     console.log("you are eligible to vote");
+// }
+
+
+
+
+    
+
